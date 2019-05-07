e2faa69fea7a38318bdbd3f3ad42f4fa6914332a
Yamil Orozco <yamiloro@gmail.com>
2019-05-06 22:08:36 -0400

    first commit

--- project0/script.js
+++ project0/script.js
@@ -10,5 +10,47 @@ const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
 function newTodo() {
-  alert('New TODO button clicked!')
+  var node = document.createElement("LI");
+  var div = document.createElement("div");
+  
+  var checkbox = document.createElement("INPUT");
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.onclick = function() {
+    uncheckedCountSpan.innerHTML = verifyCheckbox();
+  }
+
+  var label = document.createElement('label');
+  label.appendChild(document.createTextNode('TODO APP'));
+
+  var btn = document.createElement("BUTTON");
+  btn.innerHTML = "Delete";
+  btn.onclick =  function(){
+    list.removeChild(node);
+    itemCountSpan.innerText = count();
+    uncheckedCountSpan.innerText = verifyCheckbox();
+  }
+
+  label.className = classNames.TODO_TEXT;
+  checkbox.className = classNames.TODO_CHECKBOX;
+  div.appendChild(label);
+  div.appendChild(checkbox);
+  div.appendChild(btn);
+  div.className =  classNames.TODO_ITEM;
+  node.appendChild(div);
+  list.appendChild(node);  
+  uncheckedCountSpan.innerHTML = verifyCheckbox();
+
+  itemCountSpan.innerHTML = count();
 }
+
+
+function verifyCheckbox(){
+   return count() - list.querySelectorAll('input[type="checkbox"]:checked').length;
+}
+
+function count(){
+  var lilist = list.getElementsByTagName("li").length;
+  console.log(lilist);
+  return lilist;
+}
+   
